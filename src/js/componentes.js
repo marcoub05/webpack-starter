5e0e81aec1816fff9dc48c3b8958521ee99be7bb
marcoub05 <marcourreab@gmail.com>
2022-02-07 22:24:35 -0300

    Primer commit

---
+++ src/js/componentes.js
@@ -0,0 +1,15 @@
+import '../css/components.css'
+// import webpacklogo from '../assets/img/webpack-logo.png';
+
+export const saludar = (nombre)=>{
+    console.log('creando etiqueta');
+    const h1 =  document.createElement('h1');
+    h1.innerText = `Hola ${nombre}`;
+
+    document.body.append(h1);
+
+    // const img = document.createElement('img');
+    // img.src = webpacklogo;
+
+    // document.body.append(img);
+}
